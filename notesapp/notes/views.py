f39182fe77380e54f248b628da3f8e57d88e6105
dhurkadhurka <kanagadhurka237@gmail.com>
2025-02-21 14:56:44 +0530

    Add files via upload

---
+++ notesapp/notes/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Note
+from .forms import NoteForm
+
+def home(request):
+    return render(request, 'notes/home.html')
+
+@login_required
+def note_list(request):
+    notes = Note.objects.filter(user=request.user)
+    return render(request, 'notes/note_list.html', {'notes': notes})
+
+@login_required
+def create_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/note_form.html', {'form': form})
+
